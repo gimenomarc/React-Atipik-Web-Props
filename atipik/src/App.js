1e3a7bc244330221675239f458d4b81c4c5fbc3e
SirCrewete <gimenocrew1996@gmail.com>
2022-05-14 11:51:34 +0200

    Using props

--- atipik/src/App.js
+++ atipik/src/App.js
@@ -4,8 +4,45 @@ import Pizza from './components/pizza';
 function App() {
   return (
     <div className="App">
-      <h1>Atipik</h1>
-      <Pizza />
+      <h1>Menú Atipik</h1>
+      <div className='container-main'>
+        <Pizza 
+          fotoName='cuatroestaciones'
+          title='Cuatro Estaciones'
+          description='Tomate, queso, oregano, jamón, speck.'
+          price='12.50'
+        />
+        <Pizza 
+          fotoName='prosciuto'
+          title='Prosciutto'
+          description='Tomate, mozzarella y jamón york.'
+          price='13.00'
+        />
+        <Pizza 
+          fotoName='barbacoa'
+          title='Barbacoa'
+          description='Tomate, mozzarella, cebolla, bacon, pollo, ternera y salsa barbacoa.'
+          price='15.00'
+        />
+        <Pizza 
+          fotoName='cuatroquesos'
+          title='Cuatro Quesos'
+          description='Tomate, mozzarella, queso de cabra, gorgonzola y emmental.'
+          price='12.00'
+        />
+        <Pizza 
+          fotoName='dicapra'
+          title='Di Capra'
+          description='Mozzarella, cebolla caramelizada y doble queso de cabra.'
+          price='13.00'
+        />
+        <Pizza 
+          fotoName='funghi'
+          title='Funghi'
+          description='TTomate, mozzarella, jamón york y champiñones.'
+          price='13.50'
+        />
+      </div>
     </div>
   );
 }
--- atipik/src/components/pizza.jsx
+++ atipik/src/components/pizza.jsx
@@ -6,12 +6,12 @@ function Pizza(props) {
     <div className='container-pizza'>
       <img 
         className='img-pizza'
-        src={require(`../img/pizza-${props.imagen}.png`)}
+        src={require(`../img/pizza-${props.fotoName}.png`)}
         alt='Pizza foto' />
       <div className='container-text'>
-        <h1 className='title-pizza'>${props.name}</h1>
-        <p className='description-pizza'>${props.description}</p>
-        <p className='price-pizza'>${props.price}</p>
+        <h1 className='title-pizza'>{props.title}</h1>
+        <p className='description-pizza'>{props.description}</p>
+        <p className='price-pizza'>{props.price}</p>
       </div>
     </div>
   )
